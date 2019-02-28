04025a49c7ec35ba8b0bfbd9711dc03f34f605e3
Jay <jay@meangrape.com>
2019-02-28 14:53:35 -0500

    Not working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::path::Path;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+    init(&path)
 }
+
+fn init(x: &String) -> Result<(, bool)> {
+    let path = Path::new(x);
+    match path {
+        Err(e) => println!("{:?}", e),
+        _ => ()
+        }
+}
+
+#[test]
+    fn init() {
+       assert_eq!(path.to_str(), Some(".git"));
+    }
